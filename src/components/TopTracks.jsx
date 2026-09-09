f93b140f8d9ea14a459ee12b4b5cb3ce5fdee76d
D-Uribe <d.uribesandoval@gmail.com>
2026-09-08 23:58:34 -0600

    style top artists

--- src/components/TopTracks.jsx
+++ src/components/TopTracks.jsx
@@ -1,13 +1,14 @@
 export const TopTracks = ({ tunes }) => {
   return (
-    <div className="grid grid-cols-2 grid-rows-10">
-      {tunes?.items.slice(0, 10).map((song) => (
+    <div className="grid grid-cols-1 grid-rows-10  w-[500px] mx-auto">
+      {tunes?.items.slice(0, 10).map((song, index) => (
         <>
-        <div className="">
-        <img src={song.album.images[0].url} className="w-18 h-18"></img>
-          <p className="text-2xl text-blue-600" key={song.id}>
-            {song.artists[0].name}-{song.name}
-          </p>
+          <div className="flex items-center max-w-lg w-full">
+            <p>{index + 1}</p>
+            <img src={song.album.images[0].url} className="w-18 h-18"></img>
+            <p className=" text-black" key={song.id}>
+              {song.artists[0].name}-{song.name}
+            </p>
           </div>
         </>
       ))}
